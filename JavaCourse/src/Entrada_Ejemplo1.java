cd07af0d16466083693fb01f40187500c9bf12eb
dophy2706 <dophy2706@gmail.com>
2021-01-27 16:57:55 -0300

    Video 14

---
+++ JavaCourse/src/Entrada_Ejemplo1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Entrada_Ejemplo1 {
+
+	public static void main(String[] args) {
+		// Leer datos digitados por usuario 
+
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce tu Nombre, por favor: ");
+		
+		String nombreUsuario = entrada.nextLine();
+		
+		System.out.println("Introduce tu Edad, por favor: ");
+		
+		int edad = entrada.nextInt();
+		
+		System.out.println("Hola " + nombreUsuario + ", el año que viene tendrás " + (edad + 1) + " años.");
+		
+		
+	}
+
+}
